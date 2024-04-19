d39580ecc8d5147bd67e3a9185a72119238703c0
Rian Cristien <164218933+riancristien@users.noreply.github.com>
2024-04-19 00:53:06 -0300

    Add files via upload

---
+++ CalculadoraSwingg/src/calculadoraswingg/calculos.java
@@ -0,0 +1,13 @@
+
+package calculadoraswingg;
+
+public class calculos {
+
+    public static void main(String[] args) {
+        new TelaCalculadora().setVisible(true);
+        
+    }
+
+}
+    
+
